360bffbc142e0adc801620d770e491b2c0d7cfe0
Andres Garcia and Cornelius Range <cornelius@>
2016-08-10 11:09:46 -0400

    Add madlibs story

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function(event) {
+  $("#form").submit(function(event) {
+    event.preventDefault();
     var person1Input = $("input#person1").val();
     var person2Input = $("input#person2").val();
     var animalInput= $("input#animal").val();
     var exclamationInput = $("input#exclamation").val();
     var verbInput = $("input#verb").val();
     var nounInput = $("input#noun").val();
+    var spellInput = $("input#spell").val();
+    var rapInput = $("input#rap").val();
+    var introRapperInput = $("input#intro-rapper").val();
+    var introWizardInput = $("input#intro-wizard").val();
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
@@ -13,6 +18,12 @@ $(document).ready(function() {
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
     $(".noun").text(nounInput);
+    $(".spell").text(spellInput);
+    $(".rap").text(rapInput);
+    $(".intro-rapper").text(introRapperInput);
+    $(".intro-wizard").text(introWizardInput);
+
+
 
     $("#story").show();
 
